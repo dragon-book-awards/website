0e272e49be88b104a20ed23edbf08fde9d40346e
Forrest Keller <forrestblackburnkeller@gmail.com>
2020-07-20 10:49:58 +0700

    Updated footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,9 @@
 import { FC } from 'react'
+import styles from './index.module.scss'
 
 const Footer: FC = () => {
     return (
-        <footer>
+        <footer className={styles.container}>
             <small>&copy; 2020 Saigon South International School</small>
         </footer>
     )
