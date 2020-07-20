ff4ae8ffc9db27872011ce3a9c5ef66f8f9a074a
Forrest Keller <forrestblackburnkeller@gmail.com>
2020-07-20 21:38:58 +0700

    Added global styles to custom app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,20 @@
 import { AppProps } from 'next/app'
 import { FC } from 'react'
 import { Layout } from 'composites/Layout'
+import 'styles/global.scss'
 
 const CustomApp: FC<AppProps> = ({ Component, pageProps }) => {
     return (
         <Layout>
             <Component {...pageProps} />
+            <style jsx global>{`
+                * {
+                    margin: 0;
+                    padding: 0;
+                    box-sizing: border-box;
+                    font-family: 'Roboto', sans-serif;
+                }
+            `}</style>
         </Layout>
     )
 }
