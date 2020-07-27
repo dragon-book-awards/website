676569dda04ed038a8477bf3c1ca29666486d435
Forrest Keller <forrestblackburnkeller@gmail.com>
2020-07-27 22:15:10 +0700

    Add home page book image grid

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,25 @@
 import { InferGetServerSidePropsType } from 'next'
 import { contentfulClient, contentful } from 'services'
-import { InfoBlock, HeaderBlock, CompetitionPreview, Grid } from 'components'
+import shuffle from 'shuffle-array'
+import { InfoBlock, HeaderBlock, HomeSplash } from 'components'
 import { RichText } from 'composites'
 import Head from 'next/head'
 
 const HomePage = ({
-    currentCompetition,
-    archivedCompeitions,
-    homePageInfo
+    homePageInfo,
+    homeSplashImageUrls
 }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
     return (
         <>
             <Head>
                 <title>Dragon Book Awards</title>
             </Head>
+            <HomeSplash imageUrls={homeSplashImageUrls} />
             <HeaderBlock>Dragon Book Awards</HeaderBlock>
             <InfoBlock
                 title={homePageInfo.fields.title}
                 content={
-                    <>
-                        <RichText documentNode={homePageInfo.fields.content} />
-                        <br />
-                        <br />
-
-                        <CompetitionPreview
-                            id={currentCompetition.sys.id}
-                            name={currentCompetition.fields.name}
-                        />
-                    </>
+                    <RichText documentNode={homePageInfo.fields.content} />
                 }
             />
         </>
@@ -39,11 +31,41 @@ export const getServerSideProps = async () => {
         fields: { currentCompetition, archivedCompeitions, homePageInfo }
     } = await contentful.retrieveCurrentWebsite(contentfulClient)
 
+    // Get book cover image urls from current competition
+    let currentCompetitionBookCoverImageUrls = shuffle(
+        [].concat.apply(
+            [],
+            currentCompetition.fields.bookCategories.map(
+                ({ fields: { books } }) => {
+                    return books.map(
+                        ({
+                            fields: {
+                                coverImage: {
+                                    fields: {
+                                        file: { url }
+                                    }
+                                }
+                            }
+                        }) => url
+                    )
+                }
+            )
+        )
+    )
+
+    // Ensure there are enough elements in the array
+    while (currentCompetitionBookCoverImageUrls.length < 250) {
+        currentCompetitionBookCoverImageUrls = currentCompetitionBookCoverImageUrls.concat(
+            currentCompetitionBookCoverImageUrls
+        )
+    }
+
     return {
         props: {
             currentCompetition: currentCompetition,
             archivedCompeitions: archivedCompeitions || null,
-            homePageInfo: homePageInfo
+            homePageInfo: homePageInfo,
+            homeSplashImageUrls: currentCompetitionBookCoverImageUrls
         }
     }
 }
